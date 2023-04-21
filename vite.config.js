7187521b6784abf1ca3f87a0485e8ba825ec4aeb
Lasse <lassespilling@gmail.com>
2023-04-21 23:15:17 +0200

    move base to correct location

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/osrs_api_testing/",
   server: {
     port: 1337,
-    base: "/osrs_api_testing/",
     proxy: {
       "/api": {
         target: "https://secure.runescape.com",
